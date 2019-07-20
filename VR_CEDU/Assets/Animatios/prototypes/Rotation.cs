1f7981c6ee442e7b816f7b67e5b658853c977479
Juan Bojato <juandavidcancer@hotmail.com>
2019-07-20 15:29:38 -0500

    testing

---
+++ VR_CEDU/Assets/Animatios/prototypes/Rotation.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Rotation : MonoBehaviour
+{
+    // Assign in the inspector
+    public GameObject objectToRotate;
+    public Slider z;
+    public Slider y;
+    public Slider x;
+
+    // Preserve the original and current orientation
+    private float previousValue;
+    private float previousValue1;
+    private float previousValue2;
+    void Awake()
+    {
+        // Assign a callback for when this slider changes
+        this.z.onValueChanged.AddListener(this.OnSliderChanged);
+        this.y.onValueChanged.AddListener(this.OnSliderChanged1);
+        this.x.onValueChanged.AddListener(this.OnSliderChanged2);
+        // And current value
+        this.previousValue = this.z.value;
+        this.previousValue1 = this.y.value;
+        this.previousValue2 = this.x.value;
+    }
+
+    void OnSliderChanged(float value)
+    {
+        // How much we've changed
+        float delta = value - this.previousValue;
+        this.objectToRotate.transform.Rotate(Vector3.forward * delta * 360);
+
+        // Set our previous value for the next change
+        this.previousValue = value;
+    }
+
+    void OnSliderChanged1(float value)
+    {
+        // How much we've changed
+        float delta = value - this.previousValue1;
+        this.objectToRotate.transform.Rotate(Vector3.right * delta * 360);
+
+        // Set our previous value for the next change
+        this.previousValue1 = value;
+    }
+
+    void OnSliderChanged2(float value)
+    {
+        // How much we've changed
+        float delta = value - this.previousValue2;
+        this.objectToRotate.transform.Rotate(Vector3.left * delta * 360);
+
+        // Set our previous value for the next change
+        this.previousValue2 = value;
+    }
+}
---
+++ VR_CEDU/Assets/Animatios/prototypes/ScalingScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScalingScript : MonoBehaviour {
+    public Slider z;
+    public Slider y;
+    public Slider x;
+
+    private Vector3 originalScale;
+    // Use this for initialization
+    private void Start () {
+        originalScale = transform.localScale;
+    }
+
+    // Update is called once per frame
+    private void Update () {
+        transform.localScale = originalScale + new Vector3(x.value, y.value, z.value);
+    }
+}
