4f3f7e30b2d5e2f522b183a81f1bf5bfaa7e8699
Juan Bojato <juandavidcancer@hotmail.com>
2019-08-11 16:51:13 -0500

    Grid

---
+++ carte/Assets/Scripts/Line.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using UnityEngine;
+
+
+public sealed class Line : MonoBehaviour
+{
+    static readonly int[] Triangles =
+    {
+        2, 1, 0,
+        2, 3, 1,
+        6, 5, 4,
+        6, 7, 5
+    };
+
+    static readonly Vector2[] Uvs =
+    {
+        new Vector2(0, 0),
+        new Vector2(1, 0),
+        new Vector2(0, 1),
+        new Vector2(1, 1),
+        new Vector2(0, 0),
+        new Vector2(1, 0),
+        new Vector2(0, 1),
+        new Vector2(1, 1),
+    };
+
+    public Vector3 Forward = new Vector3(0, 0, -1);
+    public Vector3 Right = new Vector3(-1, 0, 0);
+    public float Width = 1;
+
+    [NonSerialized] public Vector3[] Vertices;
+    [NonSerialized] public MeshFilter Filter;
+    [NonSerialized] public Mesh Mesh;
+
+    void OnEnable()
+    {
+        Vertices = new Vector3[8];
+
+        Filter = GetComponent<MeshFilter>();
+        if (Filter == null)
+            Filter = gameObject.AddComponent<MeshFilter>();
+
+
+        Mesh = Filter.mesh;
+        if (Mesh == null)
+        {
+            Mesh = new Mesh
+            {
+                name = "DirectLine (generated)",
+                hideFlags = HideFlags.HideAndDontSave,
+                vertices = Vertices,
+                uv = Uvs,
+                triangles = Triangles,
+            };
+
+            Filter.sharedMesh = Mesh;
+        }
+    }
+
+    void OnDestroy() => Destroy(Mesh);
+
+    public void BuildMesh(Vector3 p0, Vector3 p1)
+    {
+        var delta = p0 - p1;
+
+        var offset0 = Vector3.Cross(Forward, delta);
+        offset0.Normalize();
+        offset0 *= Width;
+        Vertices[0] = p0 + offset0;
+        Vertices[1] = p0 - offset0;
+        Vertices[2] = p1 + offset0;
+        Vertices[3] = p1 - offset0;
+
+        var offset1 = Vector3.Cross(Right, delta);
+        offset1.Normalize();
+        offset1 *= Width;
+        Vertices[4] = p0 + offset1;
+        Vertices[5] = p0 - offset1;
+        Vertices[6] = p1 + offset1;
+        Vertices[7] = p1 - offset1;
+
+        // Apply vertices to mesh
+        Mesh.vertices = Vertices;
+        Mesh.uv = Uvs;
+        Mesh.triangles = Triangles;
+        Mesh.RecalculateBounds();
+        Mesh.RecalculateNormals();
+    }
+}
---
+++ carte/Assets/Scripts/Player.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+
+public sealed class Player: MonoBehaviour
+{
+	public Camera Camera;
+	public Transform Plane;
+	public Transform Point;
+	public Line OriginalLine;
+	public Line OriginalAxisLine;
+	public GameObject OriginalDot;
+	public TextMeshPro OriginalText;
+    int contador = 0;
+    int impar = 1;
+    int indice = 0;
+	// TODO: This is unused
+	[NonSerialized] public List<Point> Points;
+	[NonSerialized] public List<Line> Lines;
+   
+    void Awake()
+	{
+		Points = new List<Point>();
+		Lines = new List<Line>();
+	}
+
+	void Update()
+	{
+		var mouse = Input.mousePosition;
+		var ray = Camera.ScreenPointToRay(mouse, Camera.MonoOrStereoscopicEye.Mono);
+		var position = Raycast(Plane.transform, ray.origin, ray.direction);
+		Point.position = Plane.transform.TransformPoint(position);
+
+		if (Input.GetMouseButtonDown(0))
+		{
+			var point = new Point
+			{
+				Position = position,
+				Dot = Instantiate(OriginalDot, Plane, false),
+				AxisX = Instantiate(OriginalAxisLine, Plane, false),
+				AxisY = Instantiate(OriginalAxisLine, Plane, false),
+				Text = Instantiate(OriginalText, Plane, false),
+			};
+			point.Dot.transform.localPosition = position;
+			point.AxisX.BuildMesh(new Vector3(position.x, 0, 0), position);
+			point.AxisY.BuildMesh(new Vector3(0, position.y, 0), position);
+			point.Text.text = $"({position.x:0.0}, {position.y:0.0})";
+			point.Text.transform.localPosition = position + new Vector2(1, 1);
+
+			var index = 0;
+			//for (; index < Points.Count; index++)
+			//{
+			//	if (Points[index].Position.x > position.x)
+			//	{
+			//		Points.Insert(index, point);
+			//		break;
+			//	}
+			//}
+
+			//if (index >= Points.Count)
+
+				Points.Add(point);
+            contador++;
+            if (contador % 2==0) {
+                for (var i = impar; i < Points.Count; i++)
+                {
+
+                    var j = i - 1;
+                    if (indice >= Lines.Count)
+
+                        Lines.Add(Instantiate(OriginalLine, Plane, false));
+
+                    var line = Lines[indice];
+                    var from = Points[j].Position;
+                    var to = Points[i].Position;
+                    line.BuildMesh(from, to);
+                    line.gameObject.SetActive(true);
+                }
+
+                for (var i = Points.Count; i < Lines.Count; i++)
+                    Lines[i].gameObject.SetActive(false);
+                impar = impar + 2;
+                indice++;
+            }
+			
+		}
+	}
+
+
+	public static Vector2 Raycast(
+		Transform transform,
+		Vector3 origin,
+		Vector3 direction)
+	{
+		var localOrigin = transform.InverseTransformPoint(origin);
+		var localDirection = transform.InverseTransformDirection(direction);
+		var mul = localOrigin.z / localDirection.z;
+		return new Vector2(
+			localOrigin.x - localDirection.x * mul,
+			localOrigin.y - localDirection.y * mul);
+	}
+}
