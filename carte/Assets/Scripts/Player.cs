0751ff8e694ae96e798065ecdac13bc0d51e4909
juandavid716 <juandavidcancer@hotmail.com>
2019-08-20 11:46:12 -0500

    refactor: two hands solved

--- carte/Assets/Scripts/Player.cs
+++ carte/Assets/Scripts/Player.cs
@@ -13,7 +13,7 @@ public sealed class Player :MonoBehaviour
 	public Line OriginalAxisLine;
 	public GameObject OriginalDot;
 	public TextMeshPro OriginalText;
-  
+
     public GameObject cursorPrefab;
     public float maxCursorDistance = 30;
     private GameObject cursorInstance;
@@ -22,19 +22,19 @@ public sealed class Player :MonoBehaviour
     int indice = 0;
 	// TODO: This is unused
 	[NonSerialized] public List<Point1> Points;
-	[NonSerialized] public List<Line> Lines;
-    public Transform mano;
+    [NonSerialized] public  List<Line> Lines;
+     Transform mano;
 
      void Start()
-    {  
-      
+    {
+        mano = GameObject.Find("LeftHandAnchor").transform;
         cursorInstance = Instantiate(cursorPrefab);
     }
     void Awake()
 	{
 		Points = new List<Point1>();
-		Lines = new List<Line>();
-	}
+        Lines = new List<Line>(); ;
+    }
 
 	void Update()
 	{
@@ -45,11 +45,14 @@ public sealed class Player :MonoBehaviour
         var position = Raycast(Plane.transform, ray.origin, ray.direction);
 		Point.position = Plane.transform.TransformPoint(position);
     
-       
-            if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger))
-		{
-            Debug.DrawLine(mano.position, mano.forward);
-                var point = new Point1
+        
+        if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger) && (contador==0 ||(contador % 2==0)))
+
+        {
+            UpdateCursor();
+            
+            mano = GameObject.Find("RightHandAnchor").transform;
+            var point = new Point1
 			{
 				Position = position,
 				Dot = Instantiate(OriginalDot, Plane, false),
@@ -94,19 +97,95 @@ public sealed class Player :MonoBehaviour
                 }
 
                 for (var i = Points.Count; i < Lines.Count; i++)
-                    Lines[i].gameObject.SetActive(false);
+                    Lines[i].gameObject.SetActive(false); //debug
                 impar = impar + 2;
                 indice++;
             }
 			
-		}
-        
+		} else if (OVRInput.GetDown(OVRInput.Button.SecondaryIndexTrigger)&&  (contador % 2 != 0))
+        {
+            UpdateCursor();
+            mano = GameObject.Find("LeftHandAnchor").transform;
+            var point = new Point1
+            {
+                Position = position,
+                Dot = Instantiate(OriginalDot, Plane, false),
+                AxisX = Instantiate(OriginalAxisLine, Plane, false),
+                AxisY = Instantiate(OriginalAxisLine, Plane, false),
+                Text = Instantiate(OriginalText, Plane, false),
+            };
+            point.Dot.transform.localPosition = position;
+            point.AxisX.BuildMesh(new Vector3(position.x, 0, 0), position);
+            point.AxisY.BuildMesh(new Vector3(0, position.y, 0), position);
+            point.Text.text = $"({position.x:0.0}, {position.y:0.0})";
+            point.Text.transform.localPosition = position + new Vector2(1, 1);
+
+            var index = 0;
+            //for (; index < Points.Count; index++)
+            //{
+            //	if (Points[index].Position.x > position.x)
+            //	{
+            //		Points.Insert(index, point);
+            //		break;
+            //	}
+            //}
+
+            //if (index >= Points.Count)
+
+            Points.Add(point);
+            contador++;
+            if (contador % 2 == 0)
+            {
+                for (var i = impar; i < Points.Count; i++)
+                {
+
+                    var j = i - 1;
+                    if (indice >= Lines.Count)
+
+                        Lines.Add(Instantiate(OriginalLine, Plane, false));
+
+                    var line = Lines[indice];
+                    var from = Points[j].Position;
+                    var to = Points[i].Position;
+                    line.BuildMesh(from, to);
+                    line.gameObject.SetActive(true);
+                }
+
+                for (var i = Points.Count; i < Lines.Count; i++)
+                    Lines[i].gameObject.SetActive(false);
+                impar = impar + 2;
+                indice++;
+
+            }
+
+        }
+        if(OVRInput.GetDown(OVRInput.Button.One) || OVRInput.GetDown(OVRInput.Button.Three))
+        {
+            foreach (var item in Lines)
+            {
+                DestroyImmediate(item);
+            }
+            foreach (Point1 item in Points)
+            {
+                DestroyImmediate(item.Dot);
+                DestroyImmediate(item.Text);
+                DestroyImmediate(item.AxisX);
+                DestroyImmediate(item.AxisY);
+            }
+          
+           Lines.Clear();
+            Points.Clear();
+            Debug.Log(Points.Count);
+
+        }
     }
+           
     private void UpdateCursor()
     {
-        // Create a gaze ray pointing forward from the camera
+    
         Ray ray = new Ray(mano.position, mano.forward);
         RaycastHit hit;
+
         if (Physics.Raycast(ray, out hit, Mathf.Infinity))
         {
             // If the ray hits something, set the position to the hit point
