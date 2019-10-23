0eb68593331c9c6ebb397875a74b798cd1153a69
juandavid716 <juandavidcancer@hotmail.com>
2019-10-23 14:54:22 -0500

    get up, get down floors

--- carte/Assets/Scripts/disapear.cs
+++ carte/Assets/Scripts/disapear.cs
@@ -4,24 +4,31 @@ using UnityEngine;
 
 public class disapear : MonoBehaviour {
     public GameObject allfloors;
-    public int floornum = 0;
+    public int floornum;
+    public int maxfloors;
     private Transform[] VectoresHijos = new Transform[5];
     public CharacterController personaje;
     private Vector3 moveDirection = Vector3.zero;
     // Use this for initialization
     void Start () {
-        //allchildfloor(allfloors);
-
+        allchildfloor(allfloors);
+        maxfloors = allfloors.transform.childCount-1;
+        floornum = 1;
     }
 
     // Update is called once per frame
     void Update () {
-        //if (OVRInput.GetDown(OVRInput.Button.One))
-        //{
-        //    Movement(0);
-        //}
-       
-	}
+        if (OVRInput.GetDown(OVRInput.Button.Two)) // get up
+        {
+            Movement(floornum, 5);
+
+        }
+        else if (OVRInput.GetDown(OVRInput.Button.One)) // get down
+        {
+            Movement(floornum, 6);
+        }
+
+    }
 
     void disappearPlane(GameObject ParentP)
     {
@@ -32,19 +39,29 @@ public class disapear : MonoBehaviour {
         }
 
     }
-
-     void Movement(int floor)
+    
+     void Movement(int floor, int accion)
     {
-        if (floor == 0)
-        {
+        
+            if (floor != maxfloors && accion==5)
+            {
+                floornum = floornum + 1;
+                
             //personaje.transform.position = new Vector3(0, 10, 0);
-            personaje.enabled = false;
-            personaje.transform.position = new Vector3(personaje.transform.position.x, 6, personaje.transform.position.z);
-            personaje.enabled = true;
-        } 
-        //{
-        //    Debug.Log("sirvio" + personaje.transform);
-        //}
+                personaje.enabled = false;
+                personaje.transform.position = new Vector3(personaje.transform.position.x, VectoresHijos[floornum].transform.position.y + 1f, personaje.transform.position.z);
+                personaje.enabled = true;
+            }
+            
+            else if (floor != 0 && accion == 6)
+            {
+                floornum = floornum - 1;
+                //personaje.transform.position = new Vector3(0, 10, 0);
+                personaje.enabled = false;
+                personaje.transform.position = new Vector3(personaje.transform.position.x, VectoresHijos[floornum].transform.position.y + 1.8f, personaje.transform.position.z);
+                personaje.enabled = true;
+            }
+       
     }
 
     void allchildfloor(GameObject allf)
@@ -53,10 +70,12 @@ public class disapear : MonoBehaviour {
         {
             VectoresHijos[i] = null;
         }
-        Debug.Log(allf.transform.childCount);
+       
         for (int i = 0; i < allf.transform.childCount; i++)
         {
             VectoresHijos[i] = allf.transform.GetChild(i);
+            Debug.Log("el piso es " + VectoresHijos[i].transform.position.y);
         }
+        
     }
 }
